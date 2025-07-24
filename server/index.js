9a0525e002f672fe624f8b2d18834581b58763c8
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-07-24 15:30:00 +0530

    mongoDb added

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import connectDB from './src/services/mongodb.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -7,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+connectDB();
 
 // Routes
 app.get('/', (req, res) => {
